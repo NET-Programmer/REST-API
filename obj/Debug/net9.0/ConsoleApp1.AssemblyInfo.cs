5dbb74e3ff61dd2de0f7e7435ed1e0e02d78bc22
NET <NET@gmailcom>
2025-07-01 16:55:25 +0400

    Number 5

--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ac206b2b5f46b84a78c05b7473949a55c847a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707b589d7f04a4fbed562e23969f85ff55341919")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
